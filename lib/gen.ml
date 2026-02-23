a647d1a6b8af596958931465a15cad471aba9c82
Nicolas Koh <nclskh1@gmail.com>
2026-02-22 23:01:11 -0500

    acb.h -> flint/acb.h because arb has been merged into flint

--- lib/gen.ml
+++ lib/gen.ml
@@ -3,7 +3,7 @@ let () =
   let ml_out = open_out "arb_stub.ml" in
   output_string ml_out "(**Generated stub file. Not meant to be used by users of the arb package. *)";
   Cstubs.write_ml (Format.formatter_of_out_channel ml_out) ~prefix:"arb_stub" (module Bindings.Bindings);
-  output_string tmp_out "#include \"flint/fmpz_mat.h\"\n#include \"flint/fmpz_poly.h\"\n#include \"acb.h\"\n";
+  output_string tmp_out "#include \"flint/fmpz_mat.h\"\n#include \"flint/fmpz_poly.h\"\n#include \"flint/acb.h\"\n";
   Cstubs.write_c (Format.formatter_of_out_channel tmp_out) ~prefix:"arb_stub" (module Bindings.Bindings);
   flush tmp_out;
   flush ml_out;
